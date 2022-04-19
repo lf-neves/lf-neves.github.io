d8cd9c59a261bac9a5c929f3077b888c58f42bfe
Lucas <lucas.neves@javendeu.com>
2022-04-19 18:23:05 -0300

    Updates

---
+++ precache-manifest.17be53da861f6570eae8ac7fc98bdc62.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aff844944e036071bdcca89f2a505ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e38a6a83048edd5752",
+    "url": "/static/css/3.f55ed383.chunk.css"
+  },
+  {
+    "revision": "3526fc0afa31bd68dc59",
+    "url": "/static/css/4.84e045c1.chunk.css"
+  },
+  {
+    "revision": "279dbd191f90d269d9f3",
+    "url": "/static/css/main.2d13ba8e.chunk.css"
+  },
+  {
+    "revision": "819b847171b8304258bf",
+    "url": "/static/js/2.9f22211c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.9f22211c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e38a6a83048edd5752",
+    "url": "/static/js/3.dfb333b4.chunk.js"
+  },
+  {
+    "revision": "3526fc0afa31bd68dc59",
+    "url": "/static/js/4.9e8fc541.chunk.js"
+  },
+  {
+    "revision": "279dbd191f90d269d9f3",
+    "url": "/static/js/main.990fb234.chunk.js"
+  },
+  {
+    "revision": "3125430846d9c9ca8031",
+    "url": "/static/js/runtime-main.cdd4c72f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d03b4d02205cbe1cba2c011805cd74eb",
+    "url": "/static/media/fineduco.d03b4d02.png"
+  },
+  {
+    "revision": "27d43c75b4d43650c68a8f9091cc0367",
+    "url": "/static/media/landing-rocket@2x.27d43c75.webp"
+  },
+  {
+    "revision": "36f463dfc9a2f95196af9d848d974922",
+    "url": "/static/media/manOnTable.36f463df.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "58209b9c4672792664540f886ed8e77f",
+    "url": "/static/media/ufmg.58209b9c.jpg"
+  }
+]);
